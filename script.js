9bdeaf6a5c76aeaad833211cbcbf22da22a4ac74
Muhammad Ishaq Khan <aonemobilesncomputers786@gmail.com>
2025-09-06 08:44:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('✅ Service Worker registered with scope:', reg.scope))
     .catch(err => console.error('❌ Service Worker registration failed:', err));
 }
+
+window.addEventListener("orientationchange", function() {
+  if (window.orientation === 0 || window.orientation === 180) {
+    console.log("Portrait mode");
+  } else {
+    console.log("Landscape mode");
+  }
+});
